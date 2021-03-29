4a6b1fac610ef8b27c96bdc1e6ca8df69c8dfc17
satsuki-yabu <tetecoo.1019@gmail.com>
2021-03-29 11:10:56 +0900

    削除機能実装

--- money_book_app_flutter/lib/Calculate.dart
+++ money_book_app_flutter/lib/Calculate.dart
@@ -15,38 +15,23 @@ class Calculate extends StatefulWidget {
 
 class _CalculateState extends State<Calculate> {
 
-  // 検証用（削除する）
-  List testList = ['a','b','c'];
-  List<Map<String, String>> testMapList = [{}];
-
-
-
   // 取得した年月日
   String currentDateStr;
   // 項目名を取得
   TextEditingController _itemController = new TextEditingController();
   // 金額を取得
   TextEditingController _moneyController = new TextEditingController();
-  // インデックスを取得
-  TextEditingController _indexController = new TextEditingController();
 
   // controllerで取得した項目名と金額を入れる
   String text;
   String _itemText = '';
   String _moneyText = '';
-  int _index;
 
   // 家計簿リスト
   List<Map<String,String>> houseMoneyList = [];
 
   // 家計簿リストから繰り返し処理で配列を作成する
   Map<String,String> moneyList ={};
-
-  // 家計簿リストから繰り返し処理で配列を作成する
-  // Map<int, Map<String,String>> listChangeMap ={};
-
-  Map<int ,Map<String,String>> listChangeMap ={};
-  List<int> indexList =[];
 
   // moneyListを作成して、houseMoneyListにaddしていく
   @override
@@ -56,29 +41,17 @@ class _CalculateState extends State<Calculate> {
     DateFormat formatter = new DateFormat('yyyy/MM/dd(E)', "ja_JP");
   // currentDateStrを初期化する
     currentDateStr = formatter.format(widget.currentDate);
-
     // 初期化<String,String>
     moneyList =  {
       'item':_itemText,
       'money':_moneyText,
     };
-
-    // testMapList = [{'test':'aaa'},{'test':'bbb'},{'test':'ccc'}];
-    // houseMoneyListのindexを取得して、map型にする
-    // map型からindexのみを抜き出して、listChangeMapに入れる  listChangeMap[0,1,2...]
-    listChangeMap = houseMoneyList.asMap();
-    //  print('Index: $i' + ' Value: $value')});
   }
-
-  // indexを取得する
-  // Map<String, String>listChangeMap = houseMoneyList.asMap();
 
   //  itemView関数
   Widget itemView(value,index){
     print(value);
     print(index);
-    // print(value); //valueには{項目:食費,金額:1888}が入っている
-    // print(value['item']); //value['item']には'食費'が入っている
     return Container(
       child: Column(
         children: [
@@ -90,13 +63,11 @@ class _CalculateState extends State<Calculate> {
             shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10)
             ),
-            onPressed: () => {
-              _index = index,
-              houseMoneyList.removeAt(indexList[_index]),
-              // print(_index),
-              // print(indexList)
+            onPressed: () =>setState(()=>{
+              houseMoneyList.removeAt(index)
             }
           ),
+        ),
           RaisedButton(
             child: const Text('編集'),
             color: Colors.yellow,
@@ -140,14 +111,9 @@ class _CalculateState extends State<Calculate> {
               onPressed: () =>setState(()=>{
                 _itemText = _itemController.text,
                 _moneyText = _moneyController.text,
-                // ここで取得したitemTextとmoneyTextの値を,moneyListの中に入れる
+                // itemTextとmoneyTextの値を,moneyListの中に入れる
                 moneyList = {'item':_itemText,'money':_moneyText},
                 houseMoneyList.add(moneyList), 
-                listChangeMap.forEach((index,value)=>indexList.add(index)),
-                // リストのインデックスの配列を作成する
-                //入力を押したら,indexListに入っている
-                print(houseMoneyList),
-                print(indexList)
               }),),
               // カレンダーで取得した日付を反映する
             Text(currentDateStr),
@@ -157,10 +123,9 @@ class _CalculateState extends State<Calculate> {
                   Container(
                     child: Column(
                       children: <Widget>[ 
-                        for(int i = 0; i < houseMoneyList.length; i++)
-                          // 呼び出し先に渡す
-                          itemView(houseMoneyList[i],indexList[i]),
-                        //i番目のmoneyList{String,String}が入る
+                        for(int index = 0; index < houseMoneyList.length; index++)
+                          // 呼び先に渡す
+                          itemView(houseMoneyList[index],index),
                       ],
                     )
                   )
