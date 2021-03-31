c75bcff2f52b8db7c889c49cc33d0a392a54148d
satsuki-yabu <tetecoo.1019@gmail.com>
2021-03-31 11:23:40 +0900

    画面ごとに記録が残るように実装した

--- money_book_app_flutter/lib/Calculate.dart
+++ money_book_app_flutter/lib/Calculate.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import "package:intl/intl.dart";
-class Calculate extends StatefulWidget {
 
+class Calculate extends StatefulWidget {
   // コンストラクタ
-  Calculate({Key key, this.currentDate}) : super(key: key);
+  Calculate({
+    Key key,
+    this.currentDate,
+    this.dataList,
+    this.curHouseMoneyList,
+  }) : super(key: key);
 
   // CalenderウィジットからcurrentDateを受け取る
   // メンバ変数の宣言（クラスのスコープで使える変数の宣言）
+  // メンバ変数（フィールド）はクラス内のメソッドから参照可能な変数
   final DateTime currentDate;
+  final Map<DateTime, List<Map<String, String>>> dataList;
+  final List<Map<String, String>> curHouseMoneyList;
 
   @override
   _CalculateState createState() => _CalculateState();
 }
 
 class _CalculateState extends State<Calculate> {
-
   // 取得した年月日
   String currentDateStr;
   // 項目名を取得
@@ -34,105 +41,104 @@ class _CalculateState extends State<Calculate> {
   bool editflag;
 
   // 家計簿リストの金額の合計値
-  String strSum = '' ;
+  String strSum = '';
 
   // 家計簿リスト
-  List<Map<String,String>> houseMoneyList = [];
+  List<Map<String, String>> houseMoneyList = [];
 
   // 家計簿リストから繰り返し処理で配列を作成する
-  Map<String,String> moneyList ={};
+  Map<String, String> moneyList = {};
+
+  // Map<DateTime, List<Map<String, String>>> tempDateList = {};
 
   // moneyListを作成して、houseMoneyListにaddしていく
   @override
-  void initState(){
+  void initState() {
     super.initState();
-  // DateFormatクラスでformatterを作成する
+    // DateFormatクラスでformatterを作成する
     DateFormat formatter = new DateFormat('yyyy/MM/dd(E)', "ja_JP");
-  // currentDateStrを初期化する
+    // currentDateStrを初期化する
     currentDateStr = formatter.format(widget.currentDate);
 
+    // houseMoneyListを初期化
+    houseMoneyList = widget.curHouseMoneyList;
+    print(widget.curHouseMoneyList);
   }
-
 
   void newItem() {
-     setState(()=>{
-      _itemText = _itemController.text,
-      _moneyText = _moneyController.text,
-      // itemTextとmoneyTextの値を,moneyListの中に入れる
-      moneyList = {'item':_itemText,'money':_moneyText},
-      if( editflag != true ){   // houseMoneyListにない場合
-      houseMoneyList.add(moneyList), 
-      // 文字列を数値に変換
-      }else{ 
-        print('hoge'), 
-        houseMoneyList[editIndex] = moneyList,
-        // int.parse(moneyList['money']),
-        editflag = false
-      },
-      sumMoney()
-    });
+    setState(() => {
+          _itemText = _itemController.text,
+          _moneyText = _moneyController.text,
+          // itemTextとmoneyTextの値を,moneyListの中に入れる
+          moneyList = {'item': _itemText, 'money': _moneyText},
+          if (editflag != true)
+            {
+              // houseMoneyListにない場合
+              houseMoneyList.add(moneyList),
+              // 文字列を数値に変換
+            }
+          else
+            {
+              print('hoge'),
+              houseMoneyList[editIndex] = moneyList,
+              // int.parse(moneyList['money']),
+              editflag = false
+            },
+          sumMoney()
+        });
   }
 
-void sumMoney(){
-  List numList = [];
-  int sum =0;
-  setState(()=>{
-    for(int i = 0; i<houseMoneyList.length;i++){
-      // numList.add(int.parse(houseMoneyList[i]['money']))
-      sum +=int.parse(houseMoneyList[i]['money'])
-    },
-    // sum = numList.reduce((value, element) => value + element),
-    strSum = sum.toString(),
-    print(numList),
-    print(sum),
-  });
-}
+  void sumMoney() {
+    List numList = [];
+    int sum = 0;
+    setState(() => {
+          for (int i = 0; i < houseMoneyList.length; i++)
+            {
+              // numList.add(int.parse(houseMoneyList[i]['money']))
+              sum += int.parse(houseMoneyList[i]['money'])
+            },
+          // sum = numList.reduce((value, element) => value + element),
+          strSum = sum.toString(),
+          print(numList),
+          print(sum),
+        });
+  }
 
   //  itemView関数
-  Widget itemView(value,index){
+  Widget itemView(value, index) {
     return Container(
-      child: Column(
-        children: [
-          Text(value['item']),
-          Text(value['money']),
-          RaisedButton(
-            child: const Text('削除'),
-            color: Colors.pink,
-            shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10)
-            ),
-            onPressed: () =>setState(()=>{
+        child: Column(children: [
+      Text(value['item']),
+      Text(value['money']),
+      RaisedButton(
+        child: const Text('削除'),
+        color: Colors.pink,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        onPressed: () => setState(() => {
               houseMoneyList.removeAt(index),
               sumMoney(),
-            }
-          ),
-        ),
-          RaisedButton(
-            child: const Text('編集'),
-            color: Colors.yellow,
-            shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10)
-            ),
-            onPressed: (){ 
-              _itemController.text = value['item'];
-              _moneyController.text = value['money'];
-               editflag = true;
-               editIndex = index;
-            }
-          )
-        ]
-      )
-    );
+            }),
+      ),
+      RaisedButton(
+          child: const Text('編集'),
+          color: Colors.yellow,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          onPressed: () {
+            _itemController.text = value['item'];
+            _moneyController.text = value['money'];
+            editflag = true;
+            editIndex = index;
+          })
+    ]));
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body:
-        Container(
-        child: SingleChildScrollView(
+        resizeToAvoidBottomPadding: false,
+        body: Container(
+            child: SingleChildScrollView(
           child: Column(children: [
             TextFormField(
               //項目の入力を受けとる
@@ -143,48 +149,41 @@ void sumMoney(){
               // 金額の入力を受け取る
               controller: _moneyController,
               decoration: InputDecoration(labelText: '金額'),
-              keyboardType: TextInputType.number, 
+              keyboardType: TextInputType.number,
             ),
             // 入力値を反映する
             RaisedButton(
-              child: const Text('入力'),
-              color: Colors.green,
-              shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              ),
-              // 項目と金額を取得する
-              onPressed: ()=>newItem()
-            ),
-              // カレンダーで取得した日付を反映する
+                child: const Text('入力'),
+                color: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                // 項目と金額を取得する
+                onPressed: () => {newItem()}),
+            // カレンダーで取得した日付を反映する
             Text(currentDateStr),
-            ListView(
-              shrinkWrap: true,
-                children: [
-                  Container(
-                    child: Column(
-                      children: <Widget>[ 
-                        for(int index = 0; index < houseMoneyList.length; index++)
-                          // 呼び先に渡す
-                          itemView(houseMoneyList[index],index),
-                      ],
-                    )
-                  )
-                ]      
-            ),
+            ListView(shrinkWrap: true, children: [
+              Container(
+                  child: Column(
+                children: <Widget>[
+                  for (int index = 0; index < houseMoneyList.length; index++)
+                    // 呼び先に渡す
+                    itemView(houseMoneyList[index], index),
+                ],
+              ))
+            ]),
             Text(strSum),
             // 前の画面に戻る
             RaisedButton(
               child: const Text('保存'),
               color: Colors.purple,
               shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(10),
               ),
-              onPressed: ()=>Navigator.pop(context),
+              // 遷移元に返す値 配列houseMoneyList
+              onPressed: () => Navigator.pop(context, houseMoneyList),
             ),
-          ]
-        ),
-       )
-      )
-    );
+          ]),
+        )));
   }
 }
--- money_book_app_flutter/lib/Calender.dart
+++ money_book_app_flutter/lib/Calender.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'Calculate.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
-    show CalendarCarousel; 
+    show CalendarCarousel;
 
 class Calender extends StatefulWidget {
+  // コンストラクタ
+  // ここに、画面遷移時に渡された値が入る
+  // this.フィールド名の引数で、フィールドに値を代入できる。
+  // thisを省略すると、別の仮引数として扱われてしまう
   Calender({Key key, this.title}) : super(key: key);
 
+  // フィールド
   final String title;
 
   @override
@@ -14,29 +19,56 @@ class Calender extends StatefulWidget {
 }
 
 class _CalenderState extends State<Calender> {
+  Map<DateTime, List<Map<String, String>>> dataList = {};
+// 2021-03-23 'item':'食費'
+// 2021-03-24 'item':'日用品'
+// 関数を作成して、引数にdateを入れる
+// dateを検索する
+// 見つけたdateが同じ日であれば、呼び元にそのMapを返す
+// 呼び元がそのデータを、次のページに渡す。
 
-  
+  @override
+// ここで初期化したら、参照どころではない
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
+    return Scaffold(body: Container(
         // カレンダー表示
-        child: CalendarCarousel<Event>(  
-          // onDayPressed 日付を押したときの挙動
-           onDayPressed: (DateTime date, List<Event> events){
-          // 日付押すと、別ページに飛ぶ
-            Navigator.push(context, MaterialPageRoute(builder: (context) => new Calculate(currentDate: date))
-            );
-          }
+        child: CalendarCarousel<Event>(
+            // onDayPressed 日付を押したときの挙動
+            onDayPressed: (DateTime date, List<Event> events) async {
+      // 追加する
+      // dataListに選択した日付の情報がないとき、初期値として空の配列を入れたい
+      if (dataList.containsKey(date) == false) {
+        dataList[date] = [];
+      }
+      // dataList = {'3/12':[{'item':'項目',}],'3/13':[{'item':'項目'}]};
 
-          // 別ページには、日付、項目名、金額入力欄、入力ボタンがある
-          // 日付には、カレンダーで押した日付を出す
-          // 入力ボタンを押すと、画面下に、リストが出る
-          // １日の合計金額がでる
+      // dataList
+      // {2021/03/21 00:00:00.000:[{'item':'食費','money':'1000'}],
+      // 2021/03/22 00:00:00.000:[{'item':'日用品','money':'500'}],
+      // 2021/03/23 00:00:00.000:[{'item':'飲料水','money':'990'}]}
 
-        )
-      )
-    );
+      // houseMoneyList
+      // [{item: 食費, money: 1000},{item: 食費, money: 1000},{item: 食費, money: 1000},]
+
+      // 日付押すと、別ページに飛ぶ
+      List result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => new Calculate(
+                  currentDate: date,
+                  curHouseMoneyList: dataList[date],
+                )),
+      );
+      setState(() {
+        dataList[date] = result;
+      });
+      // print(dataList);
+      // print(result[0]['item']); //[{}]が入っている
+    })));
   }
 }
-
