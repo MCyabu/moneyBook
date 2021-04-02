14337321e46639f6285411ad4d38a822a85cfe49
satsuki-yabu <tetecoo.1019@gmail.com>
2021-04-02 10:38:34 +0900

    範囲を指定して合計金額を出す

--- money_book_app_flutter/lib/Calender.dart
+++ money_book_app_flutter/lib/Calender.dart
@@ -1,17 +1,16 @@
+// import 'dart:html';
 import 'package:flutter/material.dart';
 import 'Calculate.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel;
+import "package:intl/intl.dart";
 
 class Calender extends StatefulWidget {
-  // コンストラクタ
-  // ここに、画面遷移時に渡された値が入る
-  // this.フィールド名の引数で、フィールドに値を代入できる。
-  // thisを省略すると、別の仮引数として扱われてしまう
+
   Calender({Key key, this.title}) : super(key: key);
 
-  // フィールド
+  // フィールド変数
   final String title;
 
   @override
@@ -20,55 +19,181 @@ class Calender extends StatefulWidget {
 
 class _CalenderState extends State<Calender> {
   Map<DateTime, List<Map<String, String>>> dataList = {};
-// 2021-03-23 'item':'食費'
-// 2021-03-24 'item':'日用品'
-// 関数を作成して、引数にdateを入れる
-// dateを検索する
-// 見つけたdateが同じ日であれば、呼び元にそのMapを返す
-// 呼び元がそのデータを、次のページに渡す。
+
+  DateTime firstDay;  //範囲指定の最初の日付 （初期値は現在日、変更したら変更後の日付）
+  String strFirstDay; //firstDayを文字列に変換
+
+  DateTime lastDay; //範囲指定の最後の日付 （初期値は現在日、変更したら変更後の日付）
+  String strLastDay; //lastDayを文字列に変換
+
+  String sumMoneyOfRangeDays; //範囲指定した日付にある金額を合計
 
   @override
-// ここで初期化したら、参照どころではない
   void initState() {
     super.initState();
+
+    firstDay = DateTime.now();
+    lastDay = DateTime.now();
+
+    strFirstDay = '';
+    strLastDay = '';
+
+    sumMoneyOfRangeDays = '';
+  }
+  
+  // 最初の日付指定用のカレンダー
+   Future<Null> _selectFirstDate(BuildContext context) async {
+    
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: firstDay,
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime.now().add(new Duration(days: 360))
+    );
+    // pickedには選択した日付が入っている
+    if(picked != null) {
+      setState((){
+        firstDay = picked;
+        // 日付をString型に変換して
+        DateFormat formatter = new DateFormat('yyyy/MM/dd(E)', "ja_JP");
+        strFirstDay = formatter.format(firstDay);
+      });
+    }
   }
 
+  // 最後の日付指定用のカレンダー
+   Future<Null> _selectLastDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: lastDay,
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime.now().add(new Duration(days: 360))
+    );
+    // pickedには選択した日付が入っている
+    if(picked != null) {
+      setState((){
+        lastDay = picked;
+        // 日付をString型に変換
+        DateFormat formatter = new DateFormat('yyyy/MM/dd(E)', "ja_JP");
+        strLastDay = formatter.format(lastDay);
+        }
+      );
+    }
+  }
+
+  void getSumMoneyOfRangeDays(){
+    int differenceInDays = lastDay.difference(firstDay).inDays;
+    int sumMoney = 0;
+    // print(dataList);
+    for(int i = 0; i < differenceInDays+1 ; i++){
+      // 最初の日付に、1日ずつ足していく
+      DateTime oneDayAfterFirstDay = firstDay.add(Duration(days:i));
+      // dataListに、上記の日付がMapのkeyに含まれている場合
+      if(dataList.containsKey(oneDayAfterFirstDay) == true ){
+        for(int v = 0; v < dataList[oneDayAfterFirstDay].length; v++){
+        // Stringからintに変換する
+        int templateSumMoney = int.parse(dataList[oneDayAfterFirstDay][v]['money']);
+        sumMoney += templateSumMoney;
+        } 
+      // Stringに変換する
+      }
+    }
+    setState(()=>sumMoneyOfRangeDays = sumMoney.toString());
+    print(sumMoneyOfRangeDays);
+  }
+  // 表示
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
-        // カレンダー表示
-        child: CalendarCarousel<Event>(
-            // onDayPressed 日付を押したときの挙動
-            onDayPressed: (DateTime date, List<Event> events) async {
-      // 追加する
-      // dataListに選択した日付の情報がないとき、初期値として空の配列を入れたい
-      if (dataList.containsKey(date) == false) {
-        dataList[date] = [];
-      }
-      // dataList = {'3/12':[{'item':'項目',}],'3/13':[{'item':'項目'}]};
 
-      // dataList
-      // {2021/03/21 00:00:00.000:[{'item':'食費','money':'1000'}],
-      // 2021/03/22 00:00:00.000:[{'item':'日用品','money':'500'}],
-      // 2021/03/23 00:00:00.000:[{'item':'飲料水','money':'990'}]}
-
-      // houseMoneyList
-      // [{item: 食費, money: 1000},{item: 食費, money: 1000},{item: 食費, money: 1000},]
-
-      // 日付押すと、別ページに飛ぶ
-      List result = await Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => new Calculate(
-                  currentDate: date,
-                  curHouseMoneyList: dataList[date],
-                )),
-      );
-      setState(() {
-        dataList[date] = result;
-      });
-      // print(dataList);
-      // print(result[0]['item']); //[{}]が入っている
-    })));
+    // DateTime oneDayAfterFirstDay; // 翌日の日付。１日ずつ追加していく。
+    
+    return Scaffold(body: SingleChildScrollView(
+            child: Column(children: <Widget>[
+              Card(
+                // カレンダー表示
+                child: CalendarCarousel<Event>(
+                  height: 420.0,
+                    // onDayPressed 日付を押したときの挙動
+                    onDayPressed: (DateTime date, List<Event> events) async {
+                      // dataListに選択した日付の情報がないとき、初期値として空の配列を入れたい
+                      if (dataList.containsKey(date) == false) {
+                        dataList[date] = [];
+                      }
+                      List result = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => new Calculate(
+                                  currentDate: date,
+                                  curHouseMoneyList: dataList[date],
+                                )),
+                      );
+                      // dataListには、各日付で取得した項目と金額が入っている
+                      setState(() {
+                        dataList[date] = result;
+                      });
+                    })
+              ),
+              Row(
+                children: [
+                  // 範囲指定で最初の日付
+                  Expanded(
+                    child: Container(
+                      child:RaisedButton(
+                        child: const Text('開始'),
+                        color: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        onPressed: ()=>{_selectFirstDate(context),
+                        print(firstDay)
+                        })
+                    ),
+                  ),
+                  // 範囲指定で最後の日付
+                  Expanded(
+                    child: Container(
+                      child:RaisedButton(
+                        child: const Text('終了'),
+                        color: Colors.yellow,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        onPressed: ()=>{_selectLastDate(context),print(lastDay)})
+                    ),
+                  ),
+                  // 取得した範囲の日付で金額を合計する
+                  Expanded(
+                    child: Container(
+                      child:RaisedButton(
+                        child: const Text('合計'),
+                        color: Colors.purple,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        onPressed: ()=>{getSumMoneyOfRangeDays()}
+                      )
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text(strFirstDay)
+                    )
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text(strLastDay)
+                    )
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text(sumMoneyOfRangeDays)
+                    )
+                  ),
+                ]
+              )
+            ]
+          )
+    )
+    );
   }
 }
