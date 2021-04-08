898bf4af0b048b5b77b7f2ecd918d7aba252f301
satsuki-yabu <tetecoo.1019@gmail.com>
2021-04-08 15:51:22 +0900

    sharedPreferences実装

--- money_book_app_flutter/lib/Calender.dart
+++ money_book_app_flutter/lib/Calender.dart
@@ -1,10 +1,17 @@
+
 // import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'Calculate.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel;
 import "package:intl/intl.dart";
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+// shared preferenceが使用できるか
+// 
 
 class Calender extends StatefulWidget {
 
@@ -18,6 +25,7 @@ class Calender extends StatefulWidget {
 }
 
 class _CalenderState extends State<Calender> {
+
   Map<DateTime, List<Map<String, String>>> dataList = {};
 
   DateTime firstDay;  //範囲指定の最初の日付 （初期値は現在日、変更したら変更後の日付）
@@ -37,13 +45,134 @@ class _CalenderState extends State<Calender> {
 
     strFirstDay = '';
     strLastDay = '';
-
     sumMoneyOfRangeDays = '';
   }
+
+  // データの保存
+  Future _saveFile(BuildContext context) async {
+   SharedPreferences prefs = await SharedPreferences.getInstance(); 
+    for( DateTime key in dataList.keys) { 
+      String saveList = jsonEncode(dataList[key]);
+      // String saveList = jsonDecode(saveList);
+      // prefs.setString(key.toString(), saveList);
+      prefs.setString(key.toString(), saveList);
+    }
+  }
+
+  // データの読み込み
+   Future _readFile(BuildContext context) async {
+   SharedPreferences prefs = await SharedPreferences.getInstance(); 
+   print(prefs);
+    setState(() {
+      for( String key in prefs.getKeys()) { //保存しているデータのKeyを、変数keyに入れて、forin文で回す
+        //保存したデータのkeyをdecodeして、DateTime型にする  //2021-04-05 00:00:00.000
+        DateTime tempDataKeys = DateTime.parse(jsonDecode(jsonEncode(key))); 
+        // 保存したデータをデコードする // [{item: 食費, money: 555}]
+        final List<dynamic> tempValueList =jsonDecode(prefs.getString(key.toString()));
+
+
+        // dataList[tempDataKeys] = [{'aaa':'ccc'}]; //OK
+        // 
+        // dataList[tempDataKeys] = tempValueList; //type 'List<dynamic>' is not a subtype of type 'List<Map<String, String>>'
+        // // // 
+        //  List<Map<String, String>> sample = [tempValueList[0].cast<String, String>()]; 
+        // dataList[tempDataKeys] = sample;
+        // 日付ごとのリストを作る
+        // List<Map<String, String>>のvalueを作る
+        // 2つ合った場合、2つはいったリスト
+        // dataListのvalueに代入するリストを宣言する(sampleList)
+        List<Map<String, String>> sampleList = [] ;
+        // --->tempValueList を繰り返し処理で変数--->sampleListに入れる
+        for(int i = 0; i < tempValueList.length; i ++){
+          sampleList.add(tempValueList[i].cast<String, String>());
+          print('出力');
+          print(sampleList);
+        }
+        // dataListにtempDataKeysと、sampleListを入れる ---> dataList[temp...]=sampleList
+        dataList[tempDataKeys] = sampleList;
+        print(dataList);
+        // これを繰り返す（forin）
+          
+          // dataList[tempDataKeys] = [tempValueList[i].cast<String, String>()]; //要素が2個の場合、上書きされる
+
+     
+        
+        // type '_InternalLinkedHashMap<String, dynamic>' is not a subtype of type 'Map<String, String>' in type cast
+        // 
+        // final  tempValueList = <Map<String,String>>[jsonDecode(prefs.getString(key.toString()))];
+        // tempValueList.cast<Map<String, String>>();
+        // // (type 'List<dynamic>' is not a subtype of type 'Map<String, String>')
+        
+        // print(dataList);
+
+        
+        // final sample =  <String,dynamic>{'aaa': 123};
+        // sample.cast<String, String>();
+        // print(sample);
+        // 
+        // List<Map<String, String>>.from(tempValueList);
+        // _TypeError (type '_InternalLinkedHashMap<String, dynamic>' is not a subtype of type 'Map<String, String>')
+
+        // List<Map<String, String>>sample = tempValueList.cast<Map<String, String>>();  
+        
+        // _CastError (type '_InternalLinkedHashMap<String, dynamic>' is not a subtype of type 'Map<String, String>' in type cast)//
+        // print(tempValueList);
+
+        // dataList[tempDataKeys] = tempValueList; //type 'List<dynamic>' is not a subtype of type 'List<Map<String, String>>'
+        
+        // print(dataList[tempDataKeys]);
+
+      //  print(a);
+
+        // print(dataList[tempDataKeys]); 
+
+      //   // 一度sampleListを作って、変換して代入を試す
+      //  List<dynamic> sampleList = [{'item': '食費', 'money': '555'}];
+      //   Map<DateTime, List<Map<String, String>>> sample = sampleList.cast<List<DateTime, List<Map<String, String>>>>();
+      //   print(sampleList);
+        // Map型に追加するのは
+        // 変数名[key] = value
+        // これでしようとすると、List<dynamic>はList<Map<String, String>>じゃないというエラーがでる
+        // dataList[tempDataKeys]= tempValueList;
+        // tempValueListからMap型をとりだして、dataListのvalueにいれればよいのでは
+        // tempValueListに入っているMap型はMap<String,dynamic>なので、できませんよというエラーが出る
+        // じゃあこのMap<String,dynamic>を、Map<String,String>にしないといけない
+        // castで変換できるのは。
+        // List<dynamic>
+        // List<Map<String, String>>
+
+        // tempValueList.cast<Map<String, String>>();//できなかった
+          // dataList[tempDataKeys][0]= new Map<String,String>.from(tempValueList[0]);
+        //  Map<String,List<Map<String,String>>> sample = {'ccc':[{'aaa':'bbb'},{'eee':'rrr'}]};
+        //  sample['ccc'][1] = {'aaa':'bbb'};
+        //   print(sample);
+          // dataList[tempDataKeys][0]= tempValueList[0].cast<Map<String, String>>();
+          // print(prefs.getString(key.toString()));  // "[{"item":"食費","money":"555"}]"
+          // print(jsonDecode(prefs.getString(key.toString())));  // [{item: 食費, money: 555}]
+          // print(tempValueList[0]); // {item: 食費, money: 555}
+          // print(tempDataKeys);  //2021-04-05 00:00:00.000
+          // print(dataList); // {}
+          // print(dataList[tempDataKeys]);  //
+          // print(dataList);
+        }
+      });
+    }
   
+  // データを全削除
+   Future _removeFile(BuildContext context) async {
+   SharedPreferences prefs = await SharedPreferences.getInstance(); 
+    setState(() {
+      for( String key in prefs.getKeys()) {
+        prefs.remove(key);   
+        }
+      });
+    }
+
+
   // 最初の日付指定用のカレンダー
    Future<Null> _selectFirstDate(BuildContext context) async {
-    
+
+    //最初の日付を保存
     final DateTime picked = await showDatePicker(
         context: context,
         initialDate: firstDay,
@@ -57,12 +186,17 @@ class _CalenderState extends State<Calender> {
         // 日付をString型に変換して
         DateFormat formatter = new DateFormat('yyyy/MM/dd(E)', "ja_JP");
         strFirstDay = formatter.format(firstDay);
-      });
+        // _strFirstDay = prefs.getString(strFirstDay); 
+       }
+      );
     }
   }
 
   // 最後の日付指定用のカレンダー
    Future<Null> _selectLastDate(BuildContext context) async {
+    // 最後の日付を保存
+    // final SharedPreferences prefs = await SharedPreferences.getInstance();
+
     final DateTime picked = await showDatePicker(
         context: context,
         initialDate: lastDay,
@@ -81,6 +215,7 @@ class _CalenderState extends State<Calender> {
     }
   }
 
+// 日付を範囲指定して、合計金額を出す
   void getSumMoneyOfRangeDays(){
     int differenceInDays = lastDay.difference(firstDay).inDays;
     int sumMoney = 0;
@@ -95,18 +230,15 @@ class _CalenderState extends State<Calender> {
         int templateSumMoney = int.parse(dataList[oneDayAfterFirstDay][v]['money']);
         sumMoney += templateSumMoney;
         } 
-      // Stringに変換する
       }
     }
     setState(()=>sumMoneyOfRangeDays = sumMoney.toString());
-    print(sumMoneyOfRangeDays);
   }
+
   // 表示
   @override
   Widget build(BuildContext context) {
-
     // DateTime oneDayAfterFirstDay; // 翌日の日付。１日ずつ追加していく。
-    
     return Scaffold(body: SingleChildScrollView(
             child: Column(children: <Widget>[
               Card(
@@ -116,9 +248,11 @@ class _CalenderState extends State<Calender> {
                     // onDayPressed 日付を押したときの挙動
                     onDayPressed: (DateTime date, List<Event> events) async {
                       // dataListに選択した日付の情報がないとき、初期値として空の配列を入れたい
-                      if (dataList.containsKey(date) == false) {
+                      // if (dataList.containsKey(date) == false) {  //dataList基準
+                      if (dataList.containsKey(date) == false) {  //saveDate基準
                         dataList[date] = [];
                       }
+                      // 日付を押した時に、中にデータが入っていたら、それを表示する
                       List result = await Navigator.push(
                         context,
                         MaterialPageRoute(
@@ -133,64 +267,106 @@ class _CalenderState extends State<Calender> {
                       });
                     })
               ),
-              Row(
-                children: [
-                  // 範囲指定で最初の日付
-                  Expanded(
-                    child: Container(
-                      child:RaisedButton(
-                        child: const Text('開始'),
-                        color: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        onPressed: ()=>{_selectFirstDate(context),
-                        print(firstDay)
-                        })
+              Column (children: [
+                Row(
+                  children: [
+                    // 範囲指定で最初の日付
+                    Expanded(
+                      child: Container(
+                        child:RaisedButton(
+                          child: const Text('開始'),
+                          color: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: ()=>{_selectFirstDate(context),
+                          })
+                      ),
                     ),
-                  ),
-                  // 範囲指定で最後の日付
-                  Expanded(
-                    child: Container(
-                      child:RaisedButton(
-                        child: const Text('終了'),
-                        color: Colors.yellow,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        onPressed: ()=>{_selectLastDate(context),print(lastDay)})
+                    // 範囲指定で最後の日付
+                    Expanded(
+                      child: Container(
+                        child:RaisedButton(
+                          child: const Text('終了'),
+                          color: Colors.yellow,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: ()=>{_selectLastDate(context),
+                          })
+                      ),
                     ),
-                  ),
-                  // 取得した範囲の日付で金額を合計する
-                  Expanded(
-                    child: Container(
-                      child:RaisedButton(
-                        child: const Text('合計'),
-                        color: Colors.purple,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        onPressed: ()=>{getSumMoneyOfRangeDays()}
+                    // 取得した範囲の日付で金額を合計する
+                    Expanded(
+                      child: Container(
+                        child:RaisedButton(
+                          child: const Text('合計'),
+                          color: Colors.purple,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: ()=>{getSumMoneyOfRangeDays()}
+                        )
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text(strFirstDay)
                       )
                     ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Text(strFirstDay)
-                    )
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Text(strLastDay)
-                    )
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Text(sumMoneyOfRangeDays)
-                    )
-                  ),
-                ]
-              )
+                    Expanded(
+                      child: Container(
+                        child: Text(strLastDay)
+                      )
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text(sumMoneyOfRangeDays)
+                      )
+                    ),
+                  ]
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child:RaisedButton(
+                          child: const Text('データ保存'),
+                          color: Colors.purple,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: ()=>_saveFile(context)
+                        )
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child:RaisedButton(
+                          child: const Text('データ読み込み'),
+                          color: Colors.purple,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: ()=>_readFile(context)
+                        )
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child:RaisedButton(
+                          child: const Text('データ消去'),
+                          color: Colors.purple,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: ()=>_removeFile(context)
+                        )
+                      ),
+                    ),
+                  ] 
+                )
+              ],)
             ]
           )
     )
